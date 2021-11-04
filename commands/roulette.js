19983420d8d2a287585de4c405a553db23aaf9d5
Hirble <christianhoopert@gmail.com>
2021-11-04 15:30:56 -0400

    roulette changes

--- commands/roulette.js
+++ commands/roulette.js
@@ -7,12 +7,18 @@ module.exports = {
             return Math.floor(Math.random() * max);
         }
         chamber = getRandomInt(6)
+        console.log(chamber);
         if(chamber === 5){
             message.channel.send("https://cdn.discordapp.com/attachments/360103749844336640/897063405070987264/Alakablam.mp4")
             message.channel.send("uh oh *(gunshots are loud please wear proper hearing protection)*")
         }
         else{
+            if(message.author.username != "cosy"){
             message.channel.send("**[CLICK]** you lived this time, feel free to try again")
+            }
+            else {
+                message.channel.send("**[CLICK]** hmph.")
+            }
         }
     }
   }
