971619312c6bb41dcdf8db065c46faa15272e43e
Andres Long <andreslong95@gmail.com>
2021-11-06 21:43:22 -0500

    auto delete added for roulette

--- commands/roulette.js
+++ commands/roulette.js
@@ -3,11 +3,15 @@ let deadArr = new Map()
 
 const spinCylinder = (message) => {
     if (remainChamber > 1) {
-        return message.channel.send("these odds aren't even that bad just pull the trigger")
+        return message.channel.send("these odds aren't even that bad just pull the trigger").then(msg => {
+            msg.delete({ timeout: 7000 })
+        })
     }
     else {
         remainChamber = 6
-        return message.channel.send('alway rember wen u wer 2 scare to pull the fuckign trigger <:Rember:363188112056254474> \n (gave the cylinder a good spin)')
+        return message.channel.send('alway rember wen u wer 2 scare to pull the fuckign trigger <:Rember:363188112056254474> \n (gave the cylinder a good spin)').then(msg => {
+            msg.delete({ timeout: 7000 })
+        })
     }
 }
 
@@ -22,7 +26,9 @@ const resDead = (message, args, client) => {
     let newRes = message.guild.members.cache.get(userId)
 
     newRes.roles.remove(deadRole)
-    message.channel.send('✧･ﾟ: *✧･ﾟ:* ***ZOP*** ✧･ﾟ: *✧･ﾟ:*\n I cast alive ur mans IV')
+    message.channel.send('✧･ﾟ: *✧･ﾟ:* ***ZOP*** ✧･ﾟ: *✧･ﾟ:*\n I cast alive ur mans IV').then(msg => {
+        msg.delete({ timeout: 7000 })
+    })
 }
 
 const massRes = (message) => {
@@ -30,10 +36,10 @@ const massRes = (message) => {
         return role.name === "Graveyard"
     })
 
-   console.log(deadArr.size)
-   
-   deadArr.forEach(async grave =>  await grave.roles.remove(deadRole));
-   message.channel.send("just stop being dead??????")
+    deadArr.forEach(async grave => await grave.roles.remove(deadRole));
+    message.channel.send("just stop being dead??????").then(msg => {
+        msg.delete({ timeout: 7000 })
+    })
 }
 
 module.exports = {
@@ -43,23 +49,27 @@ module.exports = {
     cooldown: 0,
     async execute(client, message, cmd, args) {
         if (message.channel.id == 905963181812355072) return message.channel.send("ghosts have no rights")
-        console.log(deadArr.size)
+        // console.log(deadArr.size)
         if (deadArr.size == 0) {
             deadArr = await message.guild.roles.cache.find(role => {
                 return role.name === "Graveyard"
             }).members
         }
-        console.log(deadArr)
+        // console.log(deadArr)
         if (cmd === 'roulette') {
             function getRandomInt(max) {
                 return Math.floor(Math.random() * max);
             }
             chamber = getRandomInt(remainChamber)
             // chamber = 0
-            console.log(chamber);
+            // console.log(chamber);
             if (chamber === 0) {
-                message.channel.send("https://cdn.discordapp.com/attachments/360103749844336640/897063405070987264/Alakablam.mp4")
-                message.channel.send("uh oh *(gunshots are loud please wear proper hearing protection)*")
+                message.channel.send("https://cdn.discordapp.com/attachments/360103749844336640/897063405070987264/Alakablam.mp4").then(msg => {
+                    msg.delete({ timeout: 7000 })
+                })
+                message.channel.send("uh oh *(gunshots are loud please wear proper hearing protection)*").then(msg => {
+                    msg.delete({ timeout: 7000 })
+                })
 
                 let deadRole = message.guild.roles.cache.find(role => {
                     return role.name === "Graveyard"
@@ -74,18 +84,25 @@ module.exports = {
             else {
                 remainChamber--
                 if (message.author.username != "cosy") {
-                    message.channel.send("**[CLICK]** you lived this time, feel free to try again")
+                    message.channel.send("**[CLICK]** you lived this time, feel free to try again").then(msg => {
+                        msg.delete({ timeout: 7000 })
+                    })
                 }
                 else {
                     message.channel.send("**[CLICK]** hmph.")
                 }
                 if (remainChamber > 1) {
-                    message.channel.send(`${remainChamber} chamber(s) remaining <:monakS:520414023737540617>`)
+                    message.channel.send(`${remainChamber} chamber(s) remaining <:monakS:520414023737540617>`).then(msg => {
+                        msg.delete({ timeout: 7000 })
+                    })
+
                 }
             }
         }
         else if (cmd === 'spin') spinCylinder(message)
         else if (cmd === 'res') resDead(message, args, client)
         else if (cmd === 'bigres') massRes(message)
+
+        message.delete({timeout: 10000})
     }
 }
